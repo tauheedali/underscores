289a0bcbacafed4213b85f188b942967956763c2
Tauheed Ali <tauheedali91@gmail.com>
2018-09-13 09:35:54 -0400

    Theme edits

--- template-parts/content-banner.php
+++ template-parts/content-banner.php
@@ -15,10 +15,10 @@
     </div>
     <div class="container">
         <div id="service-boxes" class="row" >
-            <div class="col-xs-3"><a href="/commercial-roofing/"><img src="http://sahlemsroofing.com//wp-content/uploads/2018/08/Commercial-icon.jpg"/><span>Commercial Roofing</span></a></div>
-            <div class="col-xs-3"><a href="/resiential-roofing/"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/Residential-icon-2.jpg"/><span>Residential Roofing</span></a></div>
-            <div class="col-xs-3"><a href="/replacement-windows/"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/window-icon.jpg"/><span>Windows/Doors/Siding</span></a></div>
-            <div class="col-xs-3"><a href="/gutters/"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/Gutter-icon.jpg"/><span>Gutters/Leaf Relief</span></a></div>
+            <div class="col-xs-3"><a href="/commercial-roofing/"><img src="/wp-content/uploads/2018/08/Commercial-icon.jpg"/><span>Commercial Roofing</span></a></div>
+            <div class="col-xs-3"><a href="/resiential-roofing/"><img src="/wp-content/uploads/2018/08/Residential-icon-2.jpg"/><span>Residential Roofing</span></a></div>
+            <div class="col-xs-3"><a href="/replacement-windows/"><img src="/wp-content/uploads/2018/08/window-icon.jpg"/><span>Windows/Doors/Siding</span></a></div>
+            <div class="col-xs-3"><a href="/gutters/"><img src="/wp-content/uploads/2018/08/Gutter-icon.jpg"/><span>Gutters/Leaf Relief</span></a></div>
         </div>
     </div>
 </section>
--- template-parts/content-why-us.php
+++ template-parts/content-why-us.php
@@ -7,37 +7,8 @@
  * @package sahlems
  */
 ?>
-<section class="why-us" style="background: url('<?php the_field('why_us'); ?>');">
+<section class="why-us" style="background-image: url('<?php the_field('why_us_background'); ?>'); background-size:cover; background-repeat:no-repeat">
     <div class="container">
-		<?php //the_field('why_us_content'); ?>
-        <div id="why-us">
-            <h4>WHY US?</h4>
-            <div><p>Here are just some of the lessons we’ve learned over the years:</p></div>
-            <div class="row">
-                <div class="col-xs-2"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/802463.png"></div>
-                <div class="col-xs-10"><b>Customer satisfaction is the key.</b> You are the ultimate judge of the job we do. If you aren’t satisfied, then we won’t be satisfied either.</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-2"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/802466.png"></div>
-                <div class="col-xs-10"><b>Home and business owners deserve straight and honest dealings.</b> We provide free, no-hassle estimates for every job we perform.</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-2"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/802460.png"></div>
-                <div class="col-xs-10"><b>Your time is valuable.</b> We pledge to complete every job, commercial and residential alike, on time and on budget, and with as little inconvenience to you as possible.</div>
-            </div>
-            <div class="row">
-                <div class="col-xs-2"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/802457.png"></div>
-                <div class="col-xs-10"><b>The work we do is only as good as the people doing it.</b> Unlike other roofing companies, we don’t use subcontractors. We have a full-time staff of professional installers who
-                    perform
-                    every job. Every roofing or siding contractor you see on your site is part of our company and is ultimately accountable to you, the customer.
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-2"><img src="http://sahlemsroofing.com/wp-content/uploads/2018/08/802454.png"></div>
-                <div class="col-xs-10"><b>Responsible businesses are successful businesses.</b> We’re fully licensed and insured: we’re proactive about assuming risk on our jobs, so you don’t have to worry about it.
-                </div>
-            </div>
-        </div>
-    </div>
+		<?php the_field('why_us_content'); ?>
     </div>
 </section>
